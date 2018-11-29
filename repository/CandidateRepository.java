a0dbc03de4f816fd448dbd180bda0164fbb2aa09
felipeklatte <44686192+felipeklatte@users.noreply.github.com>
2018-11-29 14:57:56 -0200

    Delete CandidateRepository.java

--- repository/CandidateRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ulbra.election.candidate.repository;
-
-import br.edu.ulbra.election.candidate.model.Candidate;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CandidateRepository extends CrudRepository<Candidate, Long> {
-    Candidate findFirstByNumberElectionAndAndElectionId(Long numberElection, Long electionId);
-}
